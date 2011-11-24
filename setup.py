6cfce90a83cd85ed3b0eaa58a6a9ff82f63f7de1
Olivier Girardot <ssaboum@gmail.com>
2011-11-24 16:24:18 +0100

    adding basic files for deployment

---
+++ setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+
+
+class mytest(test):
+    def run(self, *args, **kwargs):
+        from runtests import runtests
+        runtests()
+
+setup(
+    name = "django-gtfs",
+    packages = ["gtfs", "gtfs.migrations"],
+    version = "0.0.1",
+    description = "Django application to load/dump and more generally handle GTFS transit data specification",
+    author = "Olivier Girardot",
+    author_email = "ssaboum@gmail.com",
+    url = "https://github.com/ssaboum/django-gtfs",
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
